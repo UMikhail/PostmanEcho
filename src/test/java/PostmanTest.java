90af5d9d6122d8bdc6f68f6c004f7e3a003b0324
Михаил Уткин <mixail.rasto4u@yandex.ru>
2022-09-04 14:09:08 +0400

    Добавление проекта

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.equalTo;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanTest {
+    @Test
+    void postmanPostTest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Mikhail") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Mikhail"))
+        ;
+    }
+}
